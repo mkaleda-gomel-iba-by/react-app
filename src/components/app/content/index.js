65702f139801a6ef8654094aaee8c9b5da1a4988
mkaleda <70138682+mkaleda@users.noreply.github.com>
2021-02-09 18:42:12 +0300

    Task4_2 (#6)
    * adding readonly
    
    * refactoring task 4
    
    * refactoring task 4
    
    * refactoring task 4
    
    * move managing card state in content
    
    * refactoring
    
    * refactoring
    
    Co-authored-by: Mikalai Kaleda <Mikalai.Kaleda@ibm.com>

--- src/components/app/content/index.js
+++ src/components/app/content/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
-import Card from "./card";
+import Cards from "./cards";
 
 function Content() {
+    const [readOnly, setReadOnly] = useState(false)
+    const [cards, setCards] = useState([
+        {id: 1, header: 'Caption', body: 'Text...'},
+        {id: 2, header: 'Caption', body: 'Text...'},
+        {id: 3, header: 'Caption', body: 'Text...'},
+        {id: 4, header: 'Caption', body: 'Text...'},
+        {id: 5, header: 'Caption', body: 'Text...'},
+        {id: 6, header: 'Caption', body: 'Text...'},
+        {id: 7, header: 'Caption', body: 'Text...'},
+        {id: 8, header: 'Caption', body: 'Text...'},
+    ])
+
+    function saveCardData(cardId, tempState) {
+        let card = cards.find(card => card.id === cardId)
+        card.header = tempState.header
+        card.body = tempState.body
+        setCards(cards)
+    }
+
     return (
         <div className="content content-layout">
             <div className="container">
-                <Card />
+                <div className="cards-panel-layout">
+                    <label><input type="checkbox" onInput={() => setReadOnly(!readOnly)}/>Readonly</label>
+                </div>
+                <Cards readOnly={readOnly} cards={cards} saveCardData={saveCardData}/>
             </div>
         </div>
     )
