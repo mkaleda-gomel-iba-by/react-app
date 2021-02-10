b7f77e4f103c5b6efdf8bb447988d7842bc0057c
mkaleda <Mikalai.Kaleda@ibm.com>
2021-02-10 20:35:48 +0300

    styled components

--- src/components/app/content/index.js
+++ src/components/app/content/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './index.css';
 import Cards from "./cards";
+import styled from "styled-components";
 
 function Content() {
     const [readOnly, setReadOnly] = useState(false)
@@ -22,11 +23,56 @@ function Content() {
         setCards(cards)
     }
 
+    const Checkmark = styled.span`
+      position: absolute;
+      top: .5rem;
+      left: .5rem;
+      height: 20px;
+      width: 20px;
+      background-color: #eee;
+      border: 2px solid rebeccapurple;
+      border-radius: 5px;
+
+      &:after {
+        content: "";
+        position: absolute;
+        display: none;
+        left: 5px;
+        top: -1px;
+        width: 7px;
+        height: 15px;
+        border: solid white;
+        border-width: 0 3px 3px 0;
+        transform: rotate(45deg);
+      }
+    `
+
+    const Checkbox = styled.input.attrs(
+        {type: 'checkbox', onClick: () => setReadOnly(!readOnly), checked: readOnly})`
+      position: absolute;
+      opacity: 0;
+      cursor: pointer;
+      height: 0;
+      width: 0;
+
+      &:checked ~ ${Checkmark} {
+        background-color: rebeccapurple;
+      }
+
+      &:checked ~ ${Checkmark}:after {
+        background-color: rebeccapurple;
+      }
+
+      .cards-panel &:checked ~ ${Checkmark}:after {
+        display: block;
+      }
+    `
+
     return (
         <div className="content content-layout">
             <div className="container">
-                <div className="cards-panel-layout">
-                    <label><input type="checkbox" onInput={() => setReadOnly(!readOnly)}/>Readonly</label>
+                <div className="cards-panel cards-panel-layout">
+                    <label><Checkbox/>Readonly<Checkmark/></label>
                 </div>
                 <Cards readOnly={readOnly} cards={cards} saveCardData={saveCardData}/>
             </div>
