208ec52103460b123c28423a414ffe893369cdaf
mkaleda <Mikalai.Kaleda@ibm.com>
2021-03-05 14:24:27 +0300

    refactoring

--- src/components/app/content/cardList/index.js
+++ src/components/app/content/cardList/index.js
@@ -5,12 +5,12 @@ import Card from "./card";
 function CardList(props) {
     return (
         <div className="cards">
-            {props.cards.map(card => Card({
-                checked: props.checkedCardIds.includes(card.id),
-                cardData: card,
-                key: card.id,
-                ...props
-            }))}
+            {props.cards.map(card => <Card cardData={card}
+                                           saveCardData={props.saveCardData}
+                                           readOnly={props.readOnly}
+                                           checkedControl={props.checkedControl}
+                                           checked={props.checkedCardIds.includes(card.id)}
+                                           key={card.id}/>)}
         </div>
     )
 }
