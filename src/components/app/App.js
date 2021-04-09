5f53d48a59d910d735f748651c29b8debc5f282a
Mikalai Kaleda <Mikalai.Kaleda@ibm.com>
2021-04-09 14:01:22 +0300

    add private route

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,13 +4,14 @@ import Header from './header';
 import Content from './content';
 import LoginPage from './login';
 import NotFound from './not_found';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import SingleCard from "./content/singleCard";
 import {fetchCards} from "../../redux/actions";
 import Settings from "./settings";
 
 function App() {
     const dispatch = useDispatch();
+    const isAdmin = useSelector(state => state.user.isAdmin)
 
     useEffect(() => {
         dispatch(fetchCards());
@@ -24,7 +25,7 @@ function App() {
                 <Route path={'/'} exact component={Content} />
                 <Route path={'/login'} exact component={LoginPage} />
                 <Route path="/card/:id" component={SingleCard} />
-                <Route path="/settings" component={Settings} />
+                {isAdmin && <Route path="/settings" component={Settings} />}
                 <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
